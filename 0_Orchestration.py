728fd9f785a6fa862e7b164503cbb9756d61d9ed
J Armando C <armjorge@gmail.com>
2025-01-09 19:04:21 -0600

    Día de coding cerrado

--- 0_Orchestration.py
+++ 0_Orchestration.py
@@ -1,18 +1,15 @@
+# Librerías externas
 import os
 import sys  # Required to modify the Python path dynamically.
-
-# Add the Library folder to the Python path.
 script_directory = os.path.dirname(os.path.abspath(__file__))
 working_folder = os.path.abspath(os.path.join(script_directory, '..'))
 function_library = os.path.abspath(os.path.join(script_directory, 'Library'))
 sys.path.append(function_library)  # Add the library folder to the path.
-
+# Librerías internas
 from A_Load_Sales_dataframe import A_Load_Sales_dataframe
 from B_referencia_interna import B_generate_orden_dicts
 from Guardar_a_excel import save_df_to_excel
-
-
-  
+ 
 def main():
     # Define working folder
     input_xlsx = os.path.join(working_folder, 'Pickle_database.xlsx')
@@ -20,7 +17,8 @@ def main():
     try:
         df_sales = A_Load_Sales_dataframe(input_xlsx)
         print(df_sales.head())  # Proper syntax for calling the DataFrame head method.
-        # Generate the internal reference dictionary
+        # Generat
+        # e the internal reference dictionary
         df_referencia_interna = B_generate_orden_dicts(df_sales)
         if not df_referencia_interna.empty:
             print("\nDiccionario de Referencia Interna generado:")
